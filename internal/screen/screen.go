816845a514f4e63f6c5a57e642919084165d2f02
diegolikescode <diegoprestes.git@hotmail.com>
2025-09-02 18:11:44 -0300

    screen drawing

--- internal/screen/screen.go
+++ internal/screen/screen.go
@@ -8,28 +8,85 @@ import (
 
 type Cell struct {
 	Occupied bool
-	Type     string
+	Type     CharType
+}
+
+type CharType string
+
+const (
+	Piece              CharType = "piece"
+	Interface          CharType = "interface"
+	PermeableInterface CharType = "permeableInterface"
+	Config             CharType = "config"
+	Clear              CharType = ""
+)
+
+const (
+	MAIN_BLOCK_WIDTH_OFFSET  = 20
+	MAIN_BLOCK_HEIGHT_OFFSET = 10
+)
+
+// ANSI styles
+const (
+	bold    = "\x1b[1m"
+	cyan    = "\x1b[36m"
+	Reset   = "\x1b[0m" // \x1b[0m
+	redPink = "\x1b[38;5;125m"
+)
+
+const (
+	V = "│"
+	H = "─"
+
+	TL = "┌"
+	BL = "└"
+	TR = "┐"
+	BR = "┘"
+)
+
+func ComposeANSI(str string, styles ...string) string {
+	res := ""
+	for _, stl := range styles {
+		res += stl
+	}
+	res += str + Reset
+	return res
+}
+
+func DrawMultiChars(stdout *os.File, charType CharType, str string, row int, col int, matrix [][]Cell) {
+	for i := range str {
+		DrawChar(stdout, charType, string(str[i]), row, col+i, matrix)
+	}
 }
 
 func ClearScreen() {
 	fmt.Fprint(os.Stdout, "\x1b[?25l") // hide cursor
 	fmt.Fprint(os.Stdout, "\x1b[2J")   // clear screen
 
-	fmt.Fprint(os.Stdout, "") // clear screen
+	fmt.Fprint(os.Stdout, "") // clear screen - again
 }
 
 func MoveCursor(row, col int) {
 	fmt.Fprintf(os.Stdout, "\x1b[%d;%dH", row, col)
 }
 
-func DrawChar(stdout *os.File, char string, row, col int, matrix [][]Cell) error {
+func DrawChar(stdout *os.File, charType CharType, char string, row int, col int, matrix [][]Cell) error {
 	MoveCursor(row, col)
 	fmt.Fprintf(stdout, char)
 
-	// matrix[row][col] =
+	occupied := true
+	if charType == Clear {
+		occupied = false
+	}
+
+	if matrix != nil {
+		matrix[row][col] = Cell{Occupied: occupied, Type: charType}
+	}
 
 	return nil
 }
+
+// func DrawHoldPiece(piece)
 
 func SetupRestore() func() {
 	c := make(chan os.Signal, 1)
@@ -51,7 +108,7 @@ func SetupRestore() func() {
 // fmt.Fprint(os.Stdout, "\x1b[?25h")
 // fmt.Fprint(os.Stdout, "\x1b[?12h")
 func restoreScreen() {
-	fmt.Fprint(os.Stdout, "\x1b[0m")   // reset attributes
+	fmt.Fprint(os.Stdout, "\x1b[0m")   // Reset attributes
 	fmt.Fprint(os.Stdout, "\x1b[2J")   // clear screen
 	fmt.Fprint(os.Stdout, "\x1b[H")    // move to home
 	fmt.Fprint(os.Stdout, "\x1b[?25h") // show cursor
